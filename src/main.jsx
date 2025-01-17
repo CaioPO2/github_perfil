0999072c5e33a2fe2c80e8c3b0becaa7734568ba
Caio <caiocesarpoke2@hotmail.com>
2025-01-17 12:50:12 -0300

    resolvendo problema do vercel

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-import './global.css'
+import './Global.css'
 
 
 createRoot(document.getElementById('root')).render(
